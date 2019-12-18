eb34bffbc4103d27a1b0e58c732552135daadf47
qianyuqiao <18302996874@163.com>
2019-12-18 11:15:52 +0800

    Create localtime.c

---
+++ localtime.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<time.h>
+#include<string.h>
+int main()
+{
+       time_t timep;
+       struct tm p;
+       struct tm *dt;
+       time(&timep);
+       dt=localtime(&timep);
+       memcpy(&p,dt,sizeof(struct tm));
+      printf("%d-%d-%d %d-%d-%d\n",1900+p.tm_year,1+p.tm_mon,p.tm_mday, p.tm_hour, p.tm_min, p.tm_sec);
+return 0;
+}
