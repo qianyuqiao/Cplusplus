6dd10a9d5b1df7e4bf95575bae647748297eb078
qianyuqiao <18302996874@163.com>
2019-12-18 11:25:04 +0800

    Update localtime.c

--- localtime.c
+++ localtime.c
@@ -3,12 +3,12 @@
 #include<string.h>
 int main()
 {
-       time_t timep;
-       struct tm p;
-       struct tm *dt;
-       time(&timep);
-       dt=localtime(&timep);
-       memcpy(&p,dt,sizeof(struct tm));
-      printf("%d-%d-%d %d-%d-%d\n",1900+p.tm_year,1+p.tm_mon,p.tm_mday, p.tm_hour, p.tm_min, p.tm_sec);
-return 0;
+    time_t timep;
+    struct tm p;
+    struct tm *dt;
+    time(&timep);
+    dt=localtime(&timep);
+    memcpy(&p,dt,sizeof(struct tm));
+    printf("%d-%d-%d %d-%d-%d\n",1900+p.tm_year,1+p.tm_mon,p.tm_mday, p.tm_hour, p.tm_min, p.tm_sec;
+    return 0;
 }
